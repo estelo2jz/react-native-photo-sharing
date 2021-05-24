5dfe5cd5217e7aecde7862a27f5e329cfcb64fc3
esteloabell <manitobaaa@gmail.com>
2021-05-24 10:47:06 -0500

    Create ImageGallery.js

---
+++ src/components/ImageGallery.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const ImageGallery = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Image Album</Text>
+    </View>
+  )
+}
+
+export default ImageGallery;

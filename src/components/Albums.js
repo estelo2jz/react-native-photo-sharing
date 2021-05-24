73ff17fca6788a57436dbe6211f171506c0a3edf
esteloabell <manitobaaa@gmail.com>
2021-05-24 18:00:49 -0500

    Update Albums.js

--- src/components/Albums.js
+++ src/components/Albums.js
@@ -1,18 +1,38 @@
 import React from 'react';
-import { View, Text, Pressable } from 'react-native';
+import { View, Text, Pressable, ScrollView, StyleSheet } from 'react-native';
 // import Navigation from '../navigation';
+import theme from '../../assets/themes/index';
+import albumPage from '../../assets/data/albumPage';
+import Card from './Card';
 
 const Album = ({navigation}) => {
   return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text>Album</Text>
-      <Pressable
-        onPress={() => navigation.navigate("Shared Album")}
-      >
-        <Text>Click me</Text>
-      </Pressable>
-    </View>
+    // <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+    //   <Text>Album</Text>
+    //   <Pressable
+    //     onPress={() => navigation.navigate("Shared Album")}
+    //   >
+    //     <Text>Click me</Text>
+    //   </Pressable>
+    // </View>
+    <>
+      <ScrollView>
+        <View style={styles.albumContainer}>
+          {albumPage.map((item, index) => 
+            <View key={index}>
+              <Card item={item} />
+            </View>
+          )}
+        </View>
+      </ScrollView>
+    </>
   )
 }
+
+const styles = StyleSheet.create({
+  albumContainer: {
+    marginBottom: theme.spacing.l,
+  }
+})
 
 export default Album;

e8c2234d37a0a394df750beea0fc5507630966d9
esteloabell <manitobaaa@gmail.com>
2021-05-24 10:47:18 -0500

    Create Avatars.js

---
+++ src/components/Avatars.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const Avatars = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Avatars</Text>
+    </View>
+  )
+}
+
+export default Avatars;

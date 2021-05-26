930d50ee43901d3ba9cef84445a1adc0d2adeb11
esteloabell <manitobaaa@gmail.com>
2021-05-26 09:05:04 -0500

    Update Albums.js

--- src/components/Albums.js
+++ src/components/Albums.js
@@ -1,12 +1,26 @@
-import React from 'react';
-import { View, Text, Pressable, ScrollView, StyleSheet } from 'react-native';
+import React, {useLayoutEffect} from 'react';
+import { View, Text, Pressable, ScrollView, StyleSheet, TouchableOpacity } from 'react-native';
 // import Navigation from '../navigation';
 import theme from '../../assets/themes/index';
 import albumPage from '../../assets/data/albumPage';
 import Card from './Card';
 import Separator from './Separator';
+import Feather from '@expo/vector-icons/Feather';
 
-const Album = ({navigation}) => {
+const Album = ({navigation, navigation: { setOptions }}) => {
+
+  useLayoutEffect(() => {
+    setOptions({
+      headerRight: () => (
+        <TouchableOpacity onPress={() => console.log('Hello')}>
+          <View style={styles.openSheetButton}>
+            <Feather name='plus' size={16} color={theme.colors.white}  />
+          </View>
+        </TouchableOpacity>
+      )
+    })
+  })
+
   return (
     // <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
     //   <Text>Album</Text>
@@ -37,6 +51,15 @@ const Album = ({navigation}) => {
 const styles = StyleSheet.create({
   albumContainer: {
     marginBottom: theme.spacing.l,
+  },
+  openSheetButton: {
+    width: 32,
+    height: 32,
+    backgroundColor: theme.colors.primary,
+    marginRight: theme.spacing.m,
+    borderRadius: 16,
+    justifyContent: 'center',
+    alignItems: 'center',
   }
 })
 

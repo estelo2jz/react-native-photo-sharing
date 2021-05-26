65ee0ee9d0243b063d197a84060c2bf420c84b14
esteloabell <manitobaaa@gmail.com>
2021-05-26 07:03:22 -0500

    Update SharedAlbum.js

--- src/components/SharedAlbum.js
+++ src/components/SharedAlbum.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import {View, Text} from 'react-native';
 
-const SharedAlbum = () => {
+// when you pass something as a paramater, you will need to grab the route prop
+const SharedAlbum = ({ route }) => {
+
+  // what we call it in the Card component as a parameter
+  const {album} = route.params;
+  console.log(album);
+
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Text>Shared Album</Text>
